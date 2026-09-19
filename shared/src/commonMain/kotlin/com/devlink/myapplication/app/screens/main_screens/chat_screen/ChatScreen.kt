89aee1d219cacf6e3fec68a1ed1c492785bef249
Abdul <nuridinov.abdul@yandex.ru>
2026-09-19 22:21:12 +0300

    первая верстка мейн скринов

--- shared/src/commonMain/kotlin/com/devlink/myapplication/app/screens/main_screens/chat_screen/ChatScreen.kt
+++ shared/src/commonMain/kotlin/com/devlink/myapplication/app/screens/main_screens/chat_screen/ChatScreen.kt
@@ -1,8 +1,25 @@
 package com.devlink.myapplication.app.screens.main_screens.chat_screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.devlink.myapplication.app.screens.main_screens.home_screen.VacancyScreen
 
 @Composable
 fun ChatScreen(){
+    Box(modifier = Modifier.fillMaxSize().background(MaterialTheme.colorScheme.background)) {
+        Text("CHAT", fontSize = 40.sp)
+    }
+}
 
+@Composable
+@Preview
+fun ShowChatScreen(){
+    ChatScreen()
 }
--- shared/src/commonMain/kotlin/com/devlink/myapplication/app/screens/main_screens/home_screen/VacancyScreen.kt
+++ shared/src/commonMain/kotlin/com/devlink/myapplication/app/screens/main_screens/home_screen/VacancyScreen.kt
@@ -1,11 +1,24 @@
 package com.devlink.myapplication.app.screens.main_screens.home_screen
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun VacancyScreen(){
-    Box(modifier = Modifier.fillMaxSize())
+    Box(modifier = Modifier.fillMaxSize().background(MaterialTheme.colorScheme.background)) {
+        Text("VACANCY", fontSize = 40.sp)
+    }
+}
+
+@Composable
+@Preview
+fun ShowVacancyScreen(){
+    VacancyScreen()
 }
