1730a3352e18c8e3e16140da27907fd861824344
Abdul <nuridinov.abdul@yandex.ru>
2026-09-24 22:18:55 +0300

    при выборе нажатии на любую эконку экрана в нижней навигации она немного увеличивается

--- shared/src/commonMain/kotlin/com/devlink/myapplication/app/navigation/custom_bottom_navigation/FloatingBottomBar.kt
+++ shared/src/commonMain/kotlin/com/devlink/myapplication/app/navigation/custom_bottom_navigation/FloatingBottomBar.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
@@ -91,7 +92,7 @@ fun FloatingBottomBar(
                         Icon(
                             painter = painterResource(iconRes),
                             contentDescription = null,
-                            modifier = Modifier.size(24.dp),
+                            modifier = if(isSelected) Modifier.size(28.dp) else Modifier.size(24.dp),
                             tint = if (isSelected) Color.White else Color.Gray
                         )
                         if (isSelected) {
