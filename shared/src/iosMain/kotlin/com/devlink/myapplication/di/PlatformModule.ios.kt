8ac6e990b70a15c67dab36bea8b8e7f276f89e58
Abdul <nuridinov.abdul@yandex.ru>
2026-09-24 13:17:20 +0300

    убрал лишнее

--- shared/src/iosMain/kotlin/com/devlink/myapplication/di/PlatformModule.ios.kt
+++
@@ -1,7 +0,0 @@
-package com.devlink.myapplication.di
-
-actual val platformModule = m {
-    // Or use Compiler Plugin DSL / Annotations if no custom logic needed
-    single<IosPlatformHelper>() bind PlatformHelper::class
-    single<IosDatabaseDriver>() bind DatabaseDriver::class
-}
